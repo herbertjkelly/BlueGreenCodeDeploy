853c64f3f966dd3df788011ed9810cacf906db1c
Herbert <herbertkelly@gmail.com>
2017-02-17 08:56:47 +0200

    BG 1.1

---
+++ web/test.js
@@ -0,0 +1,5 @@
+require("http").createServer(function(request, response){
+              response.writeHeader(200, {"Content-Type": "text/plain"});
+                response.write("Hello World!");
+                  response.end();
+}).listen(8080);
